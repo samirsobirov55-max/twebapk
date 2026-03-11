e219f04c4e1e767a80614f5da0e992d07ea47542
samirsobirov55-max <samirsobirov55@gmail.com>
2026-03-11 19:24:11 +0500

    Update MainActivity.java

--- app/src/main/java/com/pulse/messenger/MainActivity.java
+++ app/src/main/java/com/pulse/messenger/MainActivity.java
@@ -1,24 +1,23 @@
-public class MainActivity extends AppCompatActivity {
-    private WebView webView;
+package com.pulse.messenger;
+import android.os.Bundle;
+import android.webkit.*;
+import androidx.appcompat.app.AppCompatActivity;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        webView = findViewById(R.id.webview);
+        WebView webView = new WebView(this);
+        setContentView(webView);
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
         webView.setWebViewClient(new WebViewClient());
-        
         webView.setWebChromeClient(new WebChromeClient() {
             @Override
             public void onPermissionRequest(PermissionRequest request) {
                 request.grant(request.getResources());
             }
         });
-
         webView.loadUrl("https://auragram-telegram-web.hf.space");
     }
 }
-
