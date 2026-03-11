b2ebf85efeef2c7b3babcf439ff6ca76200c19a1
samirsobirov55-max <samirsobirov55@gmail.com>
2026-03-11 19:20:26 +0500

    Create MainActivity.java

---
+++ app/src/main/java/com/pulse/messenger/MainActivity.java
@@ -0,0 +1,24 @@
+public class MainActivity extends AppCompatActivity {
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = findViewById(R.id.webview);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+        
+        webView.setWebChromeClient(new WebChromeClient() {
+            @Override
+            public void onPermissionRequest(PermissionRequest request) {
+                request.grant(request.getResources());
+            }
+        });
+
+        webView.loadUrl("https://auragram-telegram-web.hf.space");
+    }
+}
+
