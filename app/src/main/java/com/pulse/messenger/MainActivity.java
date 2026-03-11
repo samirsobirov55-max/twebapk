d1185e280d8a9042ee8135755db0925ea8995a35
samirsobirov55-max <samirsobirov55@gmail.com>
2026-03-11 19:39:41 +0500

    Update MainActivity.java

--- app/src/main/java/com/pulse/messenger/MainActivity.java
+++ app/src/main/java/com/pulse/messenger/MainActivity.java
@@ -1,6 +1,10 @@
 package com.pulse.messenger;
+
 import android.os.Bundle;
-import android.webkit.*;
+import android.webkit.PermissionRequest;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
